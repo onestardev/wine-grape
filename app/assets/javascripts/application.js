ab3e6cf38ad9c177eaaa94fb50dd0f852a16df94
onestardev <faonestar514@gmail.com>
2017-12-24 00:21:17 +0800

    better

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
-//= require_directory
+//= require_tree .
 //= require social-share-button

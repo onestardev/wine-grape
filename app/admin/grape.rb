5d52ad95487128a7217d6ce86e661c4a5f0d76a1
onestardev <faonestar514@gmail.com>
2017-12-26 04:46:07 +0100

    better

--- app/admin/grape.rb
+++ app/admin/grape.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Grape do
   # everything happens here :D
+  scope :all, default: true
   permit_params :name, :short_des, :logn_des, :pic, :regions, :publish, :supply, :share, :like
+
 end

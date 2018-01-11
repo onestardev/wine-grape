d3b5b10b3187ae10e8d5eef1daa893f8df6026f0
smart-web-rock <faonestar514@gmail.com>
2018-01-11 08:26:00 -0800

    update gapecontroller

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -2,13 +2,13 @@ class WinesController < InheritedResources::Base
 
 	def index
 
-		@wines = Wine.where(publish: 't')
+		@wines = Wine.where(publish: '1')
 		
 	end
 
 	def week
 
-		@wines = Wine.where(publish: 't',week_pub: 't')
+		@wines = Wine.where(publish: '1',week_pub: '1')
 
 	end
 

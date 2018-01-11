cc24a20cd684ba91ed23be4c329bc1df6633010c
smart-web-rock <faonestar514@gmail.com>
2018-01-10 18:10:35 -0800

    admin update

--- app/admin/grape.rb
+++ app/admin/grape.rb
@@ -2,5 +2,16 @@ ActiveAdmin.register Grape do
   # everything happens here :D
   scope :all, default: true
   permit_params :name, :short_des, :long_des, :pic, :regions, :publish, :supply, :share, :like
+  index do
+
+  	column :id
+  	column :name
+  	column :pic
+  	column :regions
+  	column :publish
+  	column :supply
+  	column :updated_at
+  	actions
+  end
 
 end
--- app/admin/wine.rb
+++ app/admin/wine.rb
@@ -1,4 +1,13 @@
 ActiveAdmin.register Wine do
   # everything happens here :D
   permit_params :name, :short_des, :long_des, :pic, :regions, :wine, :publish, :share, :like
+  index do
+  	column :name
+  	column :pic
+  	column :regions
+  	column :wine
+  	column :publish
+  	column :updated_at
+  	actions
+  end
 end
